a8f3a033555dc5ecc1fad193c92bea6b3cb2c560
AnthonyA21 <95726846+AnthonyA21@users.noreply.github.com>
2021-12-23 14:27:45 -0600

    Adds test to read CSV into HashMap and excludes test-output folder

--- automation/src/test/java/JavaIOTests.java
+++ automation/src/test/java/JavaIOTests.java
@@ -1,7 +1,12 @@
-import java.util.HashMap;
+import java.io.*;
+import java.util.*;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvException;
 
 import framework.ConfigurationProvider;
 
@@ -17,4 +22,30 @@ public class JavaIOTests {
 			e.printStackTrace();
 		}
 	}
+	
+	@Test
+	public void canReadCsvIntoHashMap() {
+		try {
+			CSVReader reader = new CSVReaderBuilder(new FileReader("src/test/resources/basses.csv")).withSkipLines(1).build();
+			HashMap<String, String> bassMap = new HashMap<String, String>();
+			String expectedBassModel = "BB500";
+			
+			List<String[]> allRows = null;
+			try {
+				allRows = reader.readAll();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (CsvException e1) {
+				e1.printStackTrace();
+			}
+
+			for (String[] row : allRows) {
+				bassMap.put(row[0], row[1]);
+			}
+			
+			Assert.assertEquals(bassMap.get("Yamaha"), expectedBassModel);
+		} catch (FileNotFoundException e2) {
+			e2.printStackTrace();
+		}
+	}	
 }
